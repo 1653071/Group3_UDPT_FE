26e59efa0284058a6c4e38ad6d1165aafaa7a72e
dangvuminh <dangvuminh98@gmail.com>
2021-08-08 09:22:42 +0700

    Home Page

--- user/src/App.js
+++ user/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import {BrowserRouter, Route} from 'react-router-dom'
 import Home from './pages/home';
 import SignUp from './pages/signUp';
--- user/src/pages/home/index.js
+++ user/src/pages/home/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import SideBar from '../../components/bars/sidebar'
+import TaskBar from '../../components/bars/taskbar'
+import Card from '../../components/cards'
+import "./style.css"
 
 export default function Home() {
     return (
-        <div>
-            Home
+        <div className="home">
+            <div className="home_content row">
+                <SideBar />
+                <div className="col-sm-6">
+                    <TaskBar />
+                    <Card />
+                </div>
+                <div className="col-sm-2"></div>
+            </div>
         </div>
     )
 }
